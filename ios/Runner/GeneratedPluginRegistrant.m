2c9f7851f3a5ab14e18606e94071f1299680cde8
Yagnik Chovatiya <67455233+id-48@users.noreply.github.com>
2024-05-01 09:48:42 +0530

    latest code

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -9,97 +9,73 @@
 #if __has_include(<cloud_firestore/FLTFirebaseFirestorePlugin.h>)
 #import <cloud_firestore/FLTFirebaseFirestorePlugin.h>
 #else
-
 @import cloud_firestore;
-
 #endif
 
 #if __has_include(<connectivity_plus/ConnectivityPlusPlugin.h>)
 #import <connectivity_plus/ConnectivityPlusPlugin.h>
 #else
-
 @import connectivity_plus;
-
 #endif
 
 #if __has_include(<file_picker/FilePickerPlugin.h>)
 #import <file_picker/FilePickerPlugin.h>
 #else
-
 @import file_picker;
-
 #endif
 
 #if __has_include(<file_selector_ios/FFSFileSelectorPlugin.h>)
 #import <file_selector_ios/FFSFileSelectorPlugin.h>
 #else
-
 @import file_selector_ios;
-
 #endif
 
 #if __has_include(<firebase_core/FLTFirebaseCorePlugin.h>)
 #import <firebase_core/FLTFirebaseCorePlugin.h>
 #else
-
 @import firebase_core;
-
 #endif
 
 #if __has_include(<firebase_remote_config/FLTFirebaseRemoteConfigPlugin.h>)
 #import <firebase_remote_config/FLTFirebaseRemoteConfigPlugin.h>
 #else
-
 @import firebase_remote_config;
-
 #endif
 
 #if __has_include(<fluttertoast/FluttertoastPlugin.h>)
 #import <fluttertoast/FluttertoastPlugin.h>
 #else
-
 @import fluttertoast;
-
 #endif
 
 #if __has_include(<image_picker_ios/FLTImagePickerPlugin.h>)
 #import <image_picker_ios/FLTImagePickerPlugin.h>
 #else
-
 @import image_picker_ios;
-
 #endif
 
 #if __has_include(<path_provider_foundation/PathProviderPlugin.h>)
 #import <path_provider_foundation/PathProviderPlugin.h>
 #else
-
 @import path_provider_foundation;
-
 #endif
 
 #if __has_include(<printing/PrintingPlugin.h>)
 #import <printing/PrintingPlugin.h>
 #else
-
 @import printing;
-
 #endif
 
 #if __has_include(<shared_preferences_foundation/SharedPreferencesPlugin.h>)
 #import <shared_preferences_foundation/SharedPreferencesPlugin.h>
 #else
-
 @import shared_preferences_foundation;
-
 #endif
 
 #if __has_include(<sqflite/SqflitePlugin.h>)
 #import <sqflite/SqflitePlugin.h>
 #else
-
 @import sqflite;
-
 #endif
 
 @implementation GeneratedPluginRegistrant
